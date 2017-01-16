075307f50f84c0d74f2be845c921ea8f2f887824
Liam "Andreas Hendrik" Kisters <LiamAHK@live.nl>
2017-01-16 14:02:23 +0100

    Added note class and editnote function.

---
+++ app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Note.java
@@ -0,0 +1,84 @@
+package nl.scalda.pasimo.model.employeemanagement;
+
+import java.util.Date;
+
+public class Note {
+	
+	private int id;
+	private String title;
+	private Date creationDate;
+	private String message;
+	private Teacher madeBy;
+	private Date lastEdit;
+	
+	public Note(String title, String message, Teacher madeBy) {
+		
+		this.title = title;
+		this.message = message;
+		this.madeBy = madeBy;
+		
+		this.creationDate = new Date();
+		this.lastEdit = this.creationDate;		
+		
+	}
+	
+	public Note editNote(String title, String message){
+		
+		this.title = title;
+		this.message = message;
+		
+		this.lastEdit = new Date();
+		
+		return this;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Teacher getMadeBy() {
+		return madeBy;
+	}
+
+	public void setMadeBy(Teacher madeBy) {
+		this.madeBy = madeBy;
+	}
+
+	public Date getLastEdit() {
+		return lastEdit;
+	}
+
+	public void setLastEdit(Date lastEdit) {
+		this.lastEdit = lastEdit;
+	}
+
+}
---
+++ app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Teacher.java
@@ -0,0 +1,11 @@
+package nl.scalda.pasimo.model.employeemanagement;
+
+public class Teacher {
+
+	
+	
+	public Teacher() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
