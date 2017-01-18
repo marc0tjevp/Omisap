ef9309ff77aed7bade7f4880296b36d0794ed276
Niek Flipse <nflipse@student.scalda.nl>
2017-01-18 11:10:03 +0100

    Added packages and classes for lessongroup

---
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -0,0 +1,10 @@
+package nl.scalda.pasimo.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ app/src/main/java/nl/scalda/pasimo/model/EmployeeManagement/Student.java
@@ -0,0 +1,5 @@
+package nl.scalda.pasimo.model.EmployeeManagement;
+
+public class Student {
+
+}
