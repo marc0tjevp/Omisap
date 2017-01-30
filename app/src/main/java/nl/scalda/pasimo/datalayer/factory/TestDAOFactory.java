41b13ed0ac744cb387f8d93c742bbce1f3e2a1e0
kevin G <kevindbg@hotmail.com>
2017-01-30 10:41:40 +0100

    See previous commit

--- app/src/main/java/nl/scalda/pasimo/datalayer/factory/TestDAOFactory.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/factory/TestDAOFactory.java
@@ -1,9 +1,9 @@
 package nl.scalda.pasimo.datalayer.factory;
 
-import nl.scalda.pasimo.employeeManagement.datalayer.IEducationTeamDAO;
-import nl.scalda.pasimo.employeeManagement.datalayer.ITeamDAO;
-import nl.scalda.pasimo.employeeManagement.datalayer.impl.TestEducationTeamDAO;
-import nl.scalda.pasimo.employeeManagement.datalayer.impl.TestTeamDAO;
+import nl.scalda.pasimo.datalayer.IEducationTeamDAO;
+import nl.scalda.pasimo.datalayer.ITeamDAO;
+import nl.scalda.pasimo.datalayer.impl.TestEducationTeamDAO;
+import nl.scalda.pasimo.datalayer.impl.TestTeamDAO;
 import nl.scalda.pasimo.datalayer.IDAOFactory;
 
 public class TestDAOFactory implements IDAOFactory {
