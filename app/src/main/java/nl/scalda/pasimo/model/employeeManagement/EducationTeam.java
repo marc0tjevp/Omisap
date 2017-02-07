8a8a8bae0e93cfb38b647e991d0bdbcd8723ac19
JeroenvLoon <jeroen_vloon@msn.com>
2017-02-07 13:24:38 +0100

    bugfixed educuation team

--- app/src/main/java/nl/scalda/pasimo/model/employeeManagement/EducationTeam.java
+++ app/src/main/java/nl/scalda/pasimo/model/employeeManagement/EducationTeam.java
@@ -2,11 +2,12 @@ package nl.scalda.pasimo.model.employeemanagement;
 
 import java.util.TreeSet;
 
-public class EducationTeam implements Comparable<EducationTeam>{
+public class EducationTeam implements Comparable<EducationTeam> {
 
     private String name;
     private String abbreviation;
-    private TreeSet<CoachGroup> couchGroups;
+    private TreeSet<Teacher> teachers;
+    private TreeSet<CoachGroup> coachGroups;
 
     public EducationTeam() {
     }
@@ -19,7 +20,55 @@ public class EducationTeam implements Comparable<EducationTeam>{
     public EducationTeam(String name, String abbreviation, TreeSet<CoachGroup> couchGroups) {
         this.name = name;
         this.abbreviation = abbreviation;
-        this.couchGroups = couchGroups;
+        this.coachGroups = couchGroups;
+    }
+
+    public EducationTeam(String name, String abbreviation, TreeSet<Teacher> teachers, TreeSet<CoachGroup> coachGroups) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+        this.teachers = teachers;
+        this.coachGroups = coachGroups;
+    }
+
+    public void addCouchGroup(CoachGroup coachGroup) {
+        coachGroups.add(coachGroup);
+    }
+
+    public void updateCoachGroup(CoachGroup coachGroup) {
+        for (CoachGroup ccg : coachGroups) {
+            if (coachGroup.getId() == ccg.getId()) {
+                ccg.setName(coachGroup.getName());
+                ccg.setTeacher(coachGroup.getTeacher());
+                ccg.setLessonGroups(coachGroup.getLessonGroups());
+            }
+        }
+    }
+
+    public void deleteCoachGroup(CoachGroup coachGroup) {
+        coachGroups.remove(coachGroup);
+    }
+    
+        public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public void updateTeacher(Teacher teacher) {
+        for (Teacher ct : teachers) {
+            if (teacher.getAbbreviation().equals(ct.getAbbreviation())) {
+                ct.setFirstName(teacher.getFirstName());
+                ct.setInsertion(teacher.getInsertion());
+                ct.setLastName(teacher.getLastName());
+                ct.setCardID(teacher.getCardID());
+                ct.setEmployeeNumber(teacher.getEmployeeNumber());
+                ct.setEmail(teacher.getEmail());
+                ct.setDateOfBirth(teacher.getDateOfBirth());
+                ct.setNoteList(teacher.getNoteList());
+            }
+        }
+    }
+
+    public void deleteTeacher(Teacher teacher) {
+        teachers.remove(teacher);
     }
 
     public String getName() {
@@ -38,24 +87,30 @@ public class EducationTeam implements Comparable<EducationTeam>{
         this.abbreviation = abbreviation;
     }
 
-    public TreeSet<CoachGroup> getCouchGroups() {
-        return couchGroups;
+    public TreeSet<Teacher> getTeachers() {
+        return teachers;
     }
 
-    public void setCouchGroups(TreeSet<CoachGroup> couchGroups) {
-        this.couchGroups = couchGroups;
+    public void setTeachers(TreeSet<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public TreeSet<CoachGroup> getCoachGroups() {
+        return coachGroups;
+    }
+
+    public void setCoachGroups(TreeSet<CoachGroup> coachGroups) {
+        this.coachGroups = coachGroups;
     }
 
     @Override
     public String toString() {
-        return "EducationTeam{" + "name=" + name + ", abbreviation=" + abbreviation + ", couchGroups=" + couchGroups + '}';
+        return "EducationTeam{" + "name=" + name + ", abbreviation=" + abbreviation + ", couchGroups=" + coachGroups + '}';
     }
 
     @Override
     public int compareTo(EducationTeam o) {
         return abbreviation.compareTo(o.getAbbreviation());
     }
-
-    
 
 }
