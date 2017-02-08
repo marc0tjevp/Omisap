746f5e596a60a4ae4db117da571e2eff28e03352
diederik <dnieuwenhuize1@student.scalda.nl>
2017-02-08 14:34:27 +0100

    created educationTeamController and linked view to controller

---
+++ app/src/main/java/nl/scalda/pasimo/controller/employeemanagement/EducationTeamController.java
@@ -0,0 +1,34 @@
+package nl.scalda.pasimo.controller.employeemanagement;
+
+import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.TreeSet;
+
+/**
+ * Created by Diederik on 8-2-2017.
+ */
+public class EducationTeamController extends ActionSupport {
+
+
+    public String index() {
+        return SUCCESS;
+    }
+
+
+    public String create() {
+
+        return SUCCESS;
+    }
+
+    public String delete() {
+
+        return SUCCESS;
+    }
+
+    public String update() {
+
+        return SUCCESS;
+    }
+
+}
