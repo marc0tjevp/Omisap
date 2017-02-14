6e0812fb0ba556d4ec65af239fac55f8280177cc
KNUIT\Collin <cknuit@student.scalda.nl>
2017-02-14 13:20:55 +0100

    lowercase stuff

--- app/src/main/java/nl/scalda/pasimo/controller/employeemanagement/LessonGroupController.java
+++ app/src/main/java/nl/scalda/pasimo/controller/employeemanagement/LessonGroupController.java
@@ -1,16 +1,14 @@
 package nl.scalda.pasimo.controller.employeemanagement;
 
 import com.opensymphony.xwork2.ActionSupport;
-import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
-import nl.scalda.pasimo.model.employeemanagement.Student;
 
 import java.util.TreeSet;
 
 public class LessonGroupController extends ActionSupport {
-    public int id;
-    public String name;
-    public TreeSet<Student> students = new TreeSet<>();
-    public LessonGroup lessonGroup;
+//    public int id;
+//    public String name;
+//    public TreeSet<Student> students = new TreeSet<>();
+//    public LessonGroup lessonGroup;
 
     public String execute() {
         //actions
@@ -18,6 +16,12 @@ public class LessonGroupController extends ActionSupport {
     }
 
     public String saveLessonGroup() {
+
+
+        return SUCCESS;
+    }
+    
+    public String lessonGroup() {
 
 
         return SUCCESS;
