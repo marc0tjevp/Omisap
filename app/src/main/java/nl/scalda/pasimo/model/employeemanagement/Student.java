0ac4b568868008978cd323e4470379c446b629e5
Jordy <jordy.pijpelink@hotmail.com>
2017-02-14 14:12:55 +0100

    made changes to student.java

--- app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Student.java
+++ app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Student.java
@@ -1,36 +1,33 @@
 package nl.scalda.pasimo.model.employeemanagement;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TreeSet;
 
 public class Student extends Person {
 
-    private String name;
+    private int ovNumber;
     private TreeSet<Note> noteList;
+    private GregorianCalendar cohort;
     
     public Student(){
         
     }
 
-    public Student(String name, TreeSet<Note> noteList) {
-        this.name = name;
+    public Student(TreeSet<Note> noteList) {
         this.noteList = noteList;
     }
 
-    public Student(String name, TreeSet<Note> noteList, String email, int cardID, String firstName, String insertion, String lastName, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
+    public Student(TreeSet<Note> noteList, String email, int cardID, String firstName, String insertion, String lastName, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
         super(email, cardID, firstName, insertion, lastName, yearOfBirth, monthOfBirth, dayOfBirth);
-        this.name = name;
         this.noteList = noteList;
     }
     
     
 
     public Student(String name) {
-        this.name = name;
-        noteList = new TreeSet<>();
-    }
 
-    public String getName() {
-        return this.name;
+        noteList = new TreeSet<>();
     }
 
     public TreeSet<Note> getNoteList() {
