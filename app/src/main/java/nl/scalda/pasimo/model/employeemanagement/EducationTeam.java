f3eeb84ec64a66bbdaa3c5be17a6d60b721a50f2
Wesley van Burik <wvanburik@student.scalda.nl>
2017-02-16 08:36:04 +0000

    EducationTeam.java created online with Bitbucket

---
+++ app/src/main/java/nl/scalda/pasimo/model/employeemanagement/EducationTeam.java
@@ -0,0 +1,83 @@
+package nl.scalda.pasimo.model.employeemanagement;
+
+import nl.scalda.pasimo.datalayer.factory.DAOFactory;
+
+import java.util.TreeSet;
+
+public class EducationTeam implements Comparable<EducationTeam> {
+
+	private TreeSet<Teacher> teachers;
+
+	/**
+	 * Abbreviation of the EducationTeam; e.g. AO
+	 */
+	private String abbreviation;
+
+	/**
+	 * Name of the EducationTeam; e.g. Applicatie Ontwikkelaar
+	 */
+	private String name;
+
+	public void addTeacher(Teacher t) {
+		if (teachers.add(t)) {
+			DAOFactory.getTheFactory().getDAOTeacher().add(t, this);
+		}
+
+	}
+
+	public void deleteTeacher(Teacher t) {
+		if (teachers.remove(t)) {
+			DAOFactory.getTheFactory().getDAOTeacher().delete(t, this);
+		}
+
+	}
+
+	public TreeSet<Teacher> getTeachers() {
+		return teachers;
+	}
+
+	public EducationTeam() {
+	}
+
+	public EducationTeam(String abbreviation, String name) {
+		this.abbreviation = abbreviation;
+		this.name = name;
+	}
+
+	// </editor-fold>
+
+	// <editor-fold defaultstate="collapsed" desc="getters and setters">
+
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
+	}
+
+
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// </editor-fold>
+
+	@Override
+	public int compareTo(EducationTeam o) {
+		return this.name.compareTo(o.getName());
+	
+	}
+
+
+	@Override
+	public String toString() {
+		return "EducationTeam{" + "abbreviation='" + abbreviation + '\'' + ", name='" + name + '\'' + '}';
+	}
+
+}
