4731c3aaba075c806bf74f5df406ee714835ddac
Kevin Kloet <kkloet@student.scalda.nl>
2017-03-07 13:25:41 +0100

    Added extend for MySQLDAOConnection to MYSQLDAOTeacher

--- app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOTeacher.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOTeacher.java
@@ -2,11 +2,12 @@ package nl.scalda.pasimo.datalayer.mysqldao;
 
 import java.util.TreeSet;
 
+import nl.scalda.pasimo.datalayer.factory.MySQLDAOConnection;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOTeacher;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
 import nl.scalda.pasimo.model.employeemanagement.Teacher;
 
-public class MYSQLDAOTeacher implements IDAOTeacher{
+public class MYSQLDAOTeacher extends MySQLDAOConnection implements IDAOTeacher {
 
 	@Override
 	public void create(Teacher teacher, EducationTeam team) {
@@ -28,7 +29,8 @@ public class MYSQLDAOTeacher implements IDAOTeacher{
 
 	@Override
 	public TreeSet<Teacher> readAll() {
-		// TODO Auto-generated method stub
+		//openConn();
+		
 		return null;
 	}
 
