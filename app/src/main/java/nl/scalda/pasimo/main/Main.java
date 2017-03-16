ae5e846c92d03d361d5e333d9e2a3fbee62917c3
Coen Ribbens <cribbens@student.scalda.nl>
2017-03-16 11:41:05 +0100

    Changed student and person classes with the right code and fixed errors

--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -5,9 +5,12 @@
  */
 package nl.scalda.pasimo.main;
 
+import java.util.TreeSet;
+
 import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
 import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
+import nl.scalda.pasimo.model.employeemanagement.Student;
 import nl.scalda.pasimo.model.employeemanagement.Teacher;
 import nl.scalda.pasimo.test.EducationTeamList;
 
@@ -19,16 +22,7 @@ public class Main {
 
     public static void main(String args[]) {
 
-//        LessonGroup l1 = new LessonGroup("ICO43A");
-//        LessonGroup l2 = new LessonGroup("ICO42A");
-//        LessonGroup l3 = new LessonGroup("ICO41A");
-//
-//        CoachGroup c1 = new CoachGroup(1, "A1");
-//
-//        c1.addLessonGroup(l1);
-//        c1.addLessonGroup(l2);
-//        c1.addLessonGroup(l3);
-
+    
     }
 
 }
