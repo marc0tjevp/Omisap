d47e45db2696d9f02c2637248789ef730da2e954
Jordy <jordy.pijpelink@hotmail.com>
2017-03-22 08:56:32 +0100

    model for checkin fucntioning

--- app/src/main/java/nl/scalda/pasimo/model/presenceregistration/CheckIn.java
+++ app/src/main/java/nl/scalda/pasimo/model/presenceregistration/CheckIn.java
@@ -1,11 +1,43 @@
 package nl.scalda.pasimo.model.presenceregistration;
 
+import java.util.Date;
+
 public class CheckIn {
 
 	private int logID;
+	Date date = new Date();
+	private String cardID;
 	
 	public CheckIn(){
 		
+	}
+	
+	public CheckIn(int logID, Date date, String cardID){
+		this.setLogID(logID);
+		this.setDate(date);
+		this.setCardID(cardID);
+	}
+	
+	
+	
+	
+	public int getLogID() {
+		return logID;
+	}
+	public void setLogID(int logID) {
+		this.logID = logID;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getCardID() {
+		return cardID;
+	}
+	public void setCardID(String cardID) {
+		this.cardID = cardID;
 	}
 	
 }
