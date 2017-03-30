e48c26738ab2b52fbab7d94656541ff388f55551
Niek van Dam <nvandam1@student.scalda.nl>
2017-03-30 10:08:15 +0200

    restarting US-8 datalayer because there were unsolvable errors

--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.scalda.pasimo.main;
 
+import java.util.TreeSet;
+
+import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOStudent;
+import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOTeacher;
+import nl.scalda.pasimo.datalayer.testdao.TestDAOStudent;
+import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
+import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
+import nl.scalda.pasimo.model.employeemanagement.Note;
+import nl.scalda.pasimo.model.employeemanagement.Student;
 
 /**
  *
  * @author jeroe
  */
 public class Main {
+	public static void main(String args[]) {
 
-    public static void main(String args[]) {
 
-    
-    }
+	}
 
 }
