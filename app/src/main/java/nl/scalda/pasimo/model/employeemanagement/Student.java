409e3b9d3b9d944e9f7bd2a3e90f064a0a93e745
Niek van Dam <nvandam1@student.scalda.nl>
2017-03-30 13:46:57 +0200

    fixed student (sort of)

--- app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Student.java
+++ app/src/main/java/nl/scalda/pasimo/model/employeemanagement/Student.java
@@ -2,20 +2,43 @@ package nl.scalda.pasimo.model.employeemanagement;
 
 import java.util.TreeSet;
 
+import nl.scalda.pasimo.datalayer.testdao.TestDAOStudent;
+
 public class Student extends Person {
 
-    private TreeSet<Note> noteList = new TreeSet<>();
+	private TreeSet<Note> noteList = new TreeSet<>();
 	private LessonGroup lessonGroup;
+	private int studentOV;
 	private int cohort;
-	private int coachGroupID;
-	
+	private CoachGroup coachGroup;
 
+	public Student(String email) {
 
-	public boolean deleteStudent(Student s) {
-		s = null;
-		return true;
 	}
 
+	public Student(int studentOV, int cohort, String email, String firstName, String insertion, String lastName,
+			int cardID, int yearOfBirth, TreeSet<Note> noteList, int monthOfBirth, int dayOfBirth) {
+		super(email, cardID, firstName, insertion, lastName, yearOfBirth, monthOfBirth, dayOfBirth);
+		this.studentOV = studentOV;
+		this.cohort = cohort;
+		this.noteList = noteList;
+	}
+
+	public void deleteStudent() {
+		TestDAOStudent.getInstance().delete(this);
+	}
+
+	public TreeSet<Note> getNoteList() {
+		return noteList;
+	}
+
+	public LessonGroup getLessonGroup() {
+		return lessonGroup;
+	}
+
+	public CoachGroup getCoachGroup() {
+		return coachGroup;
+	}
 
 	public int getCohort() {
 		return cohort;
@@ -25,19 +48,34 @@ public class Student extends Person {
 		this.cohort = cohort;
 	}
 
-	public int getCoachGroupID() {
-		return coachGroupID;
+	public int getStudentOV() {
+		return studentOV;
 	}
 
-	public void setCoachGroupID(int coachGroupID) {
-		this.coachGroupID = coachGroupID;
+	public void setStudentOV(int studentOV) {
+		this.studentOV = studentOV;
+	}
+
+	public String getNameOfLessonGroup() {
+		return lessonGroup.getName();
+	}
+
+	public void setLessonGroup(LessonGroup lessonGroup) {
+		this.lessonGroup = lessonGroup;
+	}
+
+	public String getNameOfCoachGroup() {
+		return coachGroup.getName();
+	}
+
+	public void setCoachGroup(CoachGroup coachGroup) {
+		this.coachGroup = coachGroup;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [noteList=" + noteList + ", lessonGroup=" + lessonGroup + ", studentOV=" + studentOV
+				+ ", cohort=" + cohort + ", coachGroup=" + coachGroup + "]";
 	}
 
 }
-
-
-
-
-	
-	
-
