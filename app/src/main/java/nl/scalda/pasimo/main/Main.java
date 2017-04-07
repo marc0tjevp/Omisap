153afbf61f63e1f20feaf13b09ccfe68a91114f9
diederik <dnieuwenhuize1@student.scalda.nl>
2017-04-07 12:51:07 +0200

    test

--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     public static void main(String args[]) {
     
-    	System.out.println("Bram heeft nooit gelijk");
+    	System.out.println("Bram nooit gelijk");
     }
 
 }
