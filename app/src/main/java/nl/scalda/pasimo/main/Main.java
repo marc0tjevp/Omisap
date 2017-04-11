e1d33cdbd938e3c5f7e32870fc0e62008493e0e6
Dhr. Dr. Prof. Em. Van Huele <bvanhuele@scalda.nl>
2017-04-11 13:03:14 +0200

    Diederik is weer raar bezig.

--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -51,6 +51,8 @@ public class Main {
     	System.out.println(CoachGroupService.getInstance().readAll());
     
     	CoachGroupService.getInstance().delete(cg);
+    	
+    	System.out.println("fkajsfskja;asfjf;ajssf;");
     }
 
 }
