f7866b94ba74ce3083e7bf4a3015d134caa3e59e
ismet <215950@student.scalda.nl>
2017-04-12 15:19:48 +0200

    started Create function in CoachGroupDAO

--- app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOCoachGroup.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOCoachGroup.java
@@ -7,9 +7,18 @@ package nl.scalda.pasimo.datalayer.mysqldao;
 
 import java.util.TreeSet;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
+
 import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
+import nl.scalda.pasimo.model.employeemanagement.Teacher;
+import nl.scalda.pasimo.service.CoachGroupService;
 
 /**
  *
@@ -17,39 +26,92 @@ import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
  */
 public class MYSQLDAOCoachGroup implements IDAOCoachGroup {
 
-    private static MYSQLDAOCoachGroup instance = null;
+	private SessionFactory factory;
+	private static MYSQLDAOCoachGroup instance = null;
 
-    @Override
-    public void create(CoachGroup CoachGroup) {
+	private MYSQLDAOCoachGroup() {
+		initialiseFactory();
+	}
 
-    }
+	private void initialiseFactory() {
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
 
-    @Override
-    public CoachGroup read(CoachGroup CoachGroup) {
-        return CoachGroup;
-    }
+	@Override
+	public void create(CoachGroup CoachGroup) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			String sql = "insert into coachgroup (name, teacher_employeenumber, educationTeam_educationTeamID)values(:name, :teacher_employeenumber, :educationTeam_educationTeamID );";
+			NativeQuery query = session.createNativeQuery(sql);
+			query.setParameter("name", CoachGroup.getName());
+			query.setParameter("teacher_employeenumber",CoachGroup.getCoach().getEmployeeNumber());
+			query.setParameter("educationTeam_educationTeamID", CoachGroupService.getInstance().getEducationTeam(CoachGroup).getId());
+			query.executeUpdate();
+			System.out.println("created");
+		} catch (HibernateException e) {
+			if(tx != null)tx.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		
+	}
 
-    @Override
-    public void update(CoachGroup coachGroup) {
+	@Override
+	public CoachGroup read(CoachGroup CoachGroup) {
+		return CoachGroup;
+	}
 
-    }
+	@Override
+	public void update(CoachGroup coachGroup) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try{
+			tx = session.beginTransaction();
+			NativeQuery query1 = session.createNativeQuery("SET foreign_keys_checks = 0;");
+			NativeQuery query2 = session.createNativeQuery("UPDATE coachgroup set name = :name, teacher_employeenumber: teacher_employeenumber, educationTeam_educationTeamID: educationTeam_educationTeamID ;");
+			NativeQuery query3 = session.createNativeQuery("SET foreign_key_checks = 1;");
+			query1.executeUpdate();
+			session.createNativeQuery("UPDATE coachgroup set name: name;")
+			.setParameter("name", coachGroup.getName())
+			.executeUpdate();
+			query2.setParameter("name", coachGroup.getName());
+			query2.executeUpdate();
+			query3.executeUpdate();
+		}catch(HibernateException e){
+			if(tx!=null)tx.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+			
+		}
 
-    @Override
-    public void delete(CoachGroup CoachGroup) {
+	}
 
-    }
+	@Override
+	public void delete(CoachGroup CoachGroup) {
 
-    @Override
+	}
+
+	@Override
 	public TreeSet<CoachGroup> readAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-    public static MYSQLDAOCoachGroup getInstance() {
-        if (instance == null) {
-            instance = new MYSQLDAOCoachGroup();
-        }
-        return instance;
-    }
+
+	public static MYSQLDAOCoachGroup getInstance() {
+		if (instance == null) {
+			instance = new MYSQLDAOCoachGroup();
+		}
+		return instance;
+	}
 
 	@Override
 	public TreeSet<CoachGroup> readAllBYTeam(EducationTeam t) {
@@ -57,7 +119,5 @@ public class MYSQLDAOCoachGroup implements IDAOCoachGroup {
 
 		return null;
 	}
-
-	
 
 }
--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -6,34 +6,55 @@
 package nl.scalda.pasimo.main;
 
 
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOEducationTeam;
+import nl.scalda.pasimo.datalayer.factory.DAOFactory;
+import nl.scalda.pasimo.datalayer.factory.MySQLDAOFactory;
+import nl.scalda.pasimo.datalayer.factory.TestDAOFactory;
+import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
+import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
 import nl.scalda.pasimo.model.employeemanagement.Teacher;
-import nl.scalda.pasimo.model.timeregistration.WorkBlock;
-import nl.scalda.pasimo.model.timeregistration.WorkingDay;
 import nl.scalda.pasimo.service.CoachGroupService;
 import nl.scalda.pasimo.service.EducationTeamService;
 
 
 /**
  *
- * @author jeroe
+ * @author Pasimo
  */
 public class Main {
 
     public static void main(String args[]) {
-    	WorkingDay w = new WorkingDay("maandag");
+    	
+    	//DAOFactory.setTheFactory(DAOFactory.getTheFactory());
+    	DAOFactory.setTheFactory(MySQLDAOFactory.getInstance());
+    	Teacher teacher = new Teacher("han", 215950, "im@student.nl");	
+    	LessonGroup lg = new LessonGroup(123, "ICO41A");
+    	CoachGroup cg = new CoachGroup("ICO", teacher);
+    	EducationTeam ed = new EducationTeam("edu", "education", 135); 
+		
     
-    	WorkBlock workblock = new WorkBlock(2, "9:30", "10:10");
+    	cg.setCoach(teacher);
+    	EducationTeamService.getInstance().getEducationTeams().add(ed);
+    	CoachGroupService.getInstance().create(cg, ed);
+    
     	
+    	
+    	
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+//    	WorkingDay w = new WorkingDay("maandag");
+//    	WorkBlock workblock = new WorkBlock(2, "9:30", "10:10");    	
 //    	w.addWorkBlock(workblock);
-    	
-
-    	
-    	w.deleteWorkBlock(workblock);
-    	
-    	
-
+//    	w.deleteWorkBlock(workblock);
     }
 
 }
