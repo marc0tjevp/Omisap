a5f1a30a46b926c8e8689284b87f19d6c0c562dc
ismet <215950@student.scalda.nl>
2017-04-18 10:38:35 +0200

    Started Create, ReadByTeam in MYSQLDAOCoachGroup

--- app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOCoachGroup.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/mysqldao/MYSQLDAOCoachGroup.java
@@ -5,6 +5,8 @@
  */
 package nl.scalda.pasimo.datalayer.mysqldao;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.TreeSet;
 
 import org.hibernate.HibernateException;
@@ -17,7 +19,6 @@ import org.hibernate.query.NativeQuery;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
-import nl.scalda.pasimo.model.employeemanagement.Teacher;
 import nl.scalda.pasimo.service.CoachGroupService;
 
 /**
@@ -48,20 +49,25 @@ public class MYSQLDAOCoachGroup implements IDAOCoachGroup {
 		Transaction tx = null;
 		try {
 			tx = session.beginTransaction();
-			String sql = "insert into coachgroup (name, teacher_employeenumber, educationTeam_educationTeamID)values(:name, :teacher_employeenumber, :educationTeam_educationTeamID );";
+			String sql = "INSERT INTO coachgroup (name, teacher_employeenumber, educationTeam_educationTeamID)values(:name , :teacher_employeenumber , :educationTeam_educationTeamID );";
 			NativeQuery query = session.createNativeQuery(sql);
 			query.setParameter("name", CoachGroup.getName());
-			query.setParameter("teacher_employeenumber",CoachGroup.getCoach().getEmployeeNumber());
-			query.setParameter("educationTeam_educationTeamID", CoachGroupService.getInstance().getEducationTeam(CoachGroup).getId());
+			query.setParameter("teacher_employeenumber", CoachGroup.getCoach().getEmployeeNumber());
+			query.setParameter("educationTeam_educationTeamID",
+					CoachGroupService.getInstance().getEducationTeam(CoachGroup).getId());
 			query.executeUpdate();
+
+			tx.commit();
+
 			System.out.println("created");
 		} catch (HibernateException e) {
-			if(tx != null)tx.rollback();
+			if (tx != null)
+				tx.rollback();
 			e.printStackTrace();
-		}finally{
+		} finally {
 			session.close();
 		}
-		
+
 	}
 
 	@Override
@@ -73,30 +79,51 @@ public class MYSQLDAOCoachGroup implements IDAOCoachGroup {
 	public void update(CoachGroup coachGroup) {
 		Session session = factory.openSession();
 		Transaction tx = null;
-		try{
+		try {
 			tx = session.beginTransaction();
 			NativeQuery query1 = session.createNativeQuery("SET foreign_keys_checks = 0;");
-			NativeQuery query2 = session.createNativeQuery("UPDATE coachgroup set name = :name, teacher_employeenumber: teacher_employeenumber, educationTeam_educationTeamID: educationTeam_educationTeamID ;");
+			NativeQuery query2 = session.createNativeQuery(
+					"UPDATE coachgroup set name = :name, teacher_employeenumber: teacher_employeenumber, educationTeam_educationTeamID: educationTeam_educationTeamID ;");
 			NativeQuery query3 = session.createNativeQuery("SET foreign_key_checks = 1;");
 			query1.executeUpdate();
-			session.createNativeQuery("UPDATE coachgroup set name: name;")
-			.setParameter("name", coachGroup.getName())
-			.executeUpdate();
+			session.createNativeQuery("UPDATE coachgroup set name: name;").setParameter("name", coachGroup.getName())
+					.executeUpdate();
 			query2.setParameter("name", coachGroup.getName());
 			query2.executeUpdate();
 			query3.executeUpdate();
-		}catch(HibernateException e){
-			if(tx!=null)tx.rollback();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
 			e.printStackTrace();
-		}finally{
+		} finally {
 			session.close();
-			
+
 		}
 
 	}
 
 	@Override
 	public void delete(CoachGroup CoachGroup) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.createNativeQuery("DELETE FROM coachgroup where name = :name , teacher_employeenumber = :teacher_employeenumber , educationTeam_educationTeamID = :educationTeam_educationTeamID ;")
+			.setParameter("name", CoachGroup.getName())
+			.setParameter("teacher_employeenumber",CoachGroup.getCoach().getEmployeeNumber())
+			.setParameter("educationTeam_educationTeamID", CoachGroupService.getInstance().getEducationTeam(CoachGroup).getId())
+			.executeUpdate();
+			session.delete(CoachGroup);
+		
+			tx.commit();
+
+			System.out.println("deleted");
+		} catch(HibernateException e) {
+			if(tx!=null)tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
 
 	}
 
@@ -115,9 +142,30 @@ public class MYSQLDAOCoachGroup implements IDAOCoachGroup {
 
 	@Override
 	public TreeSet<CoachGroup> readAllBYTeam(EducationTeam t) {
-		// TODO Auto-generated method stub
-
-		return null;
+		Session session = factory.openSession();
+		Transaction tx = null;
+		TreeSet<CoachGroup> coach = new TreeSet<>();
+		try {
+			tx = session.beginTransaction();
+			String sql = "SELECT * from coachgroup where educationTeam_educationTeamID = :educationTeamID ;";
+			NativeQuery query = session.createNativeQuery(sql);
+			query.setParameter("educationTeamID", t.getId());
+			List CoachGroupList = query.getResultList();
+			
+			for(Iterator iterator = CoachGroupList.iterator();iterator.hasNext();){
+				 Object[] obj = (Object[]) iterator.next();
+	                CoachGroup cg = new CoachGroup(String.valueOf(obj[0]));
+	                coach.add(cg);
+			 }
+		 } catch (Exception e) {
+	            if (tx != null) {
+	                tx.rollback();
+	            }
+	            e.printStackTrace();
+	        } finally {
+	            session.close();
+	        }
+	        return coach;
 	}
 
 }
--- app/src/main/java/nl/scalda/pasimo/main/Main.java
+++ app/src/main/java/nl/scalda/pasimo/main/Main.java
@@ -1,60 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.scalda.pasimo.main;
-
 
 import nl.scalda.pasimo.datalayer.factory.DAOFactory;
 import nl.scalda.pasimo.datalayer.factory.MySQLDAOFactory;
-import nl.scalda.pasimo.datalayer.factory.TestDAOFactory;
 import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
 import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
-import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
 import nl.scalda.pasimo.model.employeemanagement.Teacher;
 import nl.scalda.pasimo.service.CoachGroupService;
-import nl.scalda.pasimo.service.EducationTeamService;
 
-
-/**
- *
- * @author Pasimo
- */
 public class Main {
 
-    public static void main(String args[]) {
-    	
-    	//DAOFactory.setTheFactory(DAOFactory.getTheFactory());
-    	DAOFactory.setTheFactory(MySQLDAOFactory.getInstance());
-    	Teacher teacher = new Teacher("han", 215950, "im@student.nl");	
-    	LessonGroup lg = new LessonGroup(123, "ICO41A");
-    	CoachGroup cg = new CoachGroup("ICO", teacher);
-    	EducationTeam ed = new EducationTeam("edu", "education", 135); 
+	public static void main(String args[]) {
+
+		// DAOFactory.setTheFactory(DAOFactory.getTheFactory());
+		DAOFactory.setTheFactory(MySQLDAOFactory.getInstance());
+		Teacher teacher = new Teacher("han", 215950, "im@student.nl");
+		CoachGroup cg = new CoachGroup("abc", teacher);
+		EducationTeam ed = new EducationTeam("edu", "education", 1);
+		ed.addCoachGroup(cg);
+
+		DAOFactory.getTheFactory().getDAOCoachGroup().create(cg);
+//		Delete
+//		CoachGroupService.getInstance().delete(cg);
+
+// 		ReadbyTeam
+//		System.out.println(MYSQLDAOCoachGroup.getInstance().readAllBYTeam(ed));
 		
-    
-    	cg.setCoach(teacher);
-    	EducationTeamService.getInstance().getEducationTeams().add(ed);
-    	CoachGroupService.getInstance().create(cg, ed);
-    
-    	
-    	
-    	
+//		CREATE
+//		cg.setCoach(teacher);
+//		EducationTeamService.getInstance().getEducationTeams().add(ed);
+//		CoachGroupService.getInstance().create(cg, ed);
 
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-//    	WorkingDay w = new WorkingDay("maandag");
-//    	WorkBlock workblock = new WorkBlock(2, "9:30", "10:10");    	
-//    	w.addWorkBlock(workblock);
-//    	w.deleteWorkBlock(workblock);
-    }
-
+	}
 }
