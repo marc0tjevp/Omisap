b32d4838b30e3f19142bff638539def0868f08cd
Kevin Kloet <kkloet@student.scalda.nl>
2017-04-19 09:10:38 +0200

    fixed .gitignore and commented return MYSQLDAONote because missing class

--- app/src/main/java/nl/scalda/pasimo/datalayer/factory/MySQLDAOFactory.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/factory/MySQLDAOFactory.java
@@ -11,7 +11,7 @@ import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkingDay;
 import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOCoachGroup;
 import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOEducationTeam;
 import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOLessonGroup;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAONote;
+//import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAONote;
 import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOTeacher;
 
 
@@ -42,9 +42,11 @@ public class MySQLDAOFactory extends DAOFactory {
         return MYSQLDAOLessonGroup.getInstance();
     }
 
+    //FIXME missing class: MYSQLDAONote
     @Override
     public IDAONote getDAONote() {
-      return MYSQLDAONote.getInstance();
+    	return null;
+//      return MYSQLDAONote.getInstance();
     }
 
     @Override
