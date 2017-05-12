6fa4f4329670ae1844e2c4883b7e17921f478d59
diederik <dnieuwenhuize1@student.scalda.nl>
2017-05-12 09:24:05 +0200

    solved some more merge confrlicts

--- app/src/main/java/nl/scalda/pasimo/datalayer/factory/MySQLDAOFactory.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/factory/MySQLDAOFactory.java
@@ -4,6 +4,7 @@ import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOEducationTeam;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOLessonGroup;
 import nl.scalda.pasimo.datalayer.interfaces.IDAONote;
+import nl.scalda.pasimo.datalayer.interfaces.IDAOStudent;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOTeacher;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOTeam;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkBlock;
@@ -85,79 +86,8 @@ public class MySQLDAOFactory extends DAOFactory {
 		return MYSQLDAOWorkWeek.getInstance();
 	}
 
-}
-
-import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOEducationTeam;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOLessonGroup;
-import nl.scalda.pasimo.datalayer.interfaces.IDAONote;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOTeacher;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOTeam;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkBlock;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkingDay;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOCoachGroup;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOEducationTeam;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOLessonGroup;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAONote;
-import nl.scalda.pasimo.datalayer.mysqldao.MYSQLDAOTeacher;
-
-
-public class MySQLDAOFactory extends DAOFactory {
-
-    private static MySQLDAOFactory instance = null;
-
-    private  MySQLDAOFactory() {
-    }
-
-    public static MySQLDAOFactory getInstance() {
-        if (instance == null) {
-            instance = new MySQLDAOFactory();
-        }
-        return instance;
-    }
-
-    /**
-     * methods from interface
-     */
-    @Override
-    public IDAOCoachGroup getDAOCoachGroup() {
-        return MYSQLDAOCoachGroup.getInstance();
-    }
-
-    @Override
-    public IDAOLessonGroup getDAOLessonGroup() {
-        return MYSQLDAOLessonGroup.getInstance();
-    }
-
-    @Override
-    public IDAONote getDAONote() {
-      return MYSQLDAONote.getInstance();
-    }
-
-    @Override
-
-    public IDAOTeacher getDAOTeacher() {
-        return MYSQLDAOTeacher.getInstance();
-    }
-
-    @Override
-    public IDAOWorkingDay getDAOWorkingDay() {
-        return null;
-    }
-
-    @Override
-    public IDAOTeam getDAOTeam() {
-
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-	public IDAOEducationTeam getDAOEducationTeam() {
-		return MYSQLDAOEducationTeam.getInstance();
-
-	}
 	@Override
-	public IDAOWorkBlock getDAOWorkBlock() {
+	public IDAOStudent getDAOStudent() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- app/src/main/java/nl/scalda/pasimo/datalayer/factory/TestDAOFactory.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/factory/TestDAOFactory.java
@@ -4,6 +4,7 @@ import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOEducationTeam;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOLessonGroup;
 import nl.scalda.pasimo.datalayer.interfaces.IDAONote;
+import nl.scalda.pasimo.datalayer.interfaces.IDAOStudent;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOTeacher;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOTeam;
 import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkBlock;
@@ -83,83 +84,11 @@ public class TestDAOFactory extends DAOFactory {
 		return null;
 	}
 
-
-}
-
-import nl.scalda.pasimo.datalayer.interfaces.IDAOCoachGroup;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOEducationTeam;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOLessonGroup;
-import nl.scalda.pasimo.datalayer.interfaces.IDAONote;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOTeacher;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOTeam;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkBlock;
-import nl.scalda.pasimo.datalayer.interfaces.IDAOWorkingDay;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOCoachGroup;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOEducationTeam;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOLessonGroup;
-import nl.scalda.pasimo.datalayer.testdao.TestDAONote;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOTeacher;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOTeam;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOWorkBlock;
-import nl.scalda.pasimo.datalayer.testdao.TestDAOWorkingDay;
-
-public class TestDAOFactory extends DAOFactory {
-
-    private static TestDAOFactory instance = null;
-
-    private TestDAOFactory() {
-
-    }
-    
-    @Override
-    public IDAOTeam getDAOTeam() {
-        return TestDAOTeam.getInstance();
-    }
-
-    @Override
-    public IDAOEducationTeam getDAOEducationTeam() {
-        return TestDAOEducationTeam.getInstance();
-    }
-
-    @Override
-    public IDAOCoachGroup getDAOCoachGroup() {
-        return TestDAOCoachGroup.getInstance();
-    }
-
-    @Override
-    public IDAOLessonGroup getDAOLessonGroup() {
-        return TestDAOLessonGroup.getInstance();
-    }
-
-    @Override
-    public IDAOTeacher getDAOTeacher() {
-        return TestDAOTeacher.getInstance();
-    }
-
-    @Override
-    public IDAONote getDAONote() {
-        return TestDAONote.getInstance();
-    }
-
-    public static TestDAOFactory getInstance() {
-        if (instance == null) {
-            instance = new TestDAOFactory();
-        }
-        return instance;
-    }
-
-
 	@Override
-	public IDAOWorkingDay getDAOWorkingDay() {
-
-		return TestDAOWorkingDay.getInstance();
+	public IDAOStudent getDAOStudent() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	@Override
-	public IDAOWorkBlock getDAOWorkBlock() {
-		return TestDAOWorkBlock.getInstance();
-	
-		
-	}
 
 }
