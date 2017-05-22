f281323deac852a223c86698aabc039c14141488
Kharakosh <erinboshart@outlook.com>
2017-05-22 12:03:38 +0200

    comparable fixed

--- app/src/main/java/nl/scalda/pasimo/model/presenceregistration/CheckIn.java
+++ app/src/main/java/nl/scalda/pasimo/model/presenceregistration/CheckIn.java
@@ -2,68 +2,89 @@ package nl.scalda.pasimo.model.presenceregistration;
 
 import java.util.GregorianCalendar;
 
-public class CheckIn implements Comparable<CheckIn>{
+public class CheckIn implements Comparable<CheckIn>
+{
 
 	private int logID;
 	private int cardID;
 	private GregorianCalendar date;
-	
-	public CheckIn(){
-		
+
+	public CheckIn()
+	{
+
 	}
-	
-	public CheckIn(int logID, int cardID, int yearOfCheckIn, int monthOfCheckIn, int dayOfCheckIn, int hourOfCheckIn, int minuteOfCheckIn, int secondOfCheckIn){
+
+	public CheckIn(int logID, int cardID, int yearOfCheckIn, int monthOfCheckIn, int dayOfCheckIn, int hourOfCheckIn,
+			int minuteOfCheckIn, int secondOfCheckIn)
+	{
 		GregorianCalendar gc = new GregorianCalendar();
 		gc.set(yearOfCheckIn, monthOfCheckIn - 1, dayOfCheckIn, hourOfCheckIn, minuteOfCheckIn, secondOfCheckIn);
 		this.date = gc;
 		this.setLogID(logID);
 		this.setCardID(cardID);
-		
+
 	}
-	public String getFormattedTime(){
-		return String.valueOf(getDate().get(GregorianCalendar.HOUR_OF_DAY))	+ ":" + String.valueOf(getDate().get(GregorianCalendar.MINUTE));
+
+	public String getFormattedTime()
+	{
+		return String.valueOf(getDate().get(GregorianCalendar.HOUR_OF_DAY)) + ":"
+				+ String.valueOf(getDate().get(GregorianCalendar.MINUTE));
 	}
-	
-	
-	public String getFormattedDate(){
-		
-		return String.valueOf(getDate().get(GregorianCalendar.DAY_OF_MONTH)) + "-" 
-				+ String.valueOf(getDate().get(GregorianCalendar.MONTH)) + "-" + String.valueOf(getDate().get(GregorianCalendar.YEAR));
-				
+
+	public String getFormattedDate()
+	{
+
+		return String.valueOf(getDate().get(GregorianCalendar.DAY_OF_MONTH)) + "-"
+				+ String.valueOf(getDate().get(GregorianCalendar.MONTH)) + "-"
+				+ String.valueOf(getDate().get(GregorianCalendar.YEAR));
+
 	}
-	
-	public GregorianCalendar getDate() {
+
+	public GregorianCalendar getDate()
+	{
 		return date;
 	}
-	public void setDate(GregorianCalendar date) {
+
+	public void setDate(GregorianCalendar date)
+	{
 		this.date = date;
 	}
-	public int getLogID() {
+
+	public int getLogID()
+	{
 		return logID;
 	}
-	public void setLogID(int logID) {
+
+	public void setLogID(int logID)
+	{
 		this.logID = logID;
 	}
-	public int getCardID() {
+
+	public int getCardID()
+	{
 		return cardID;
 	}
-	public void setCardID(int cardID) {
+
+	public void setCardID(int cardID)
+	{
 		this.cardID = cardID;
 	}
-	
-    public String toString() {
-        return "CheckIn [DAY=" + this.getDate().get(GregorianCalendar.DAY_OF_MONTH) + 
-        		", Month=" + (this.getDate().get(GregorianCalendar.MONTH)+1) +
-                ", Year=" + this.getDate().get(GregorianCalendar.YEAR) + 
-                ", Hour=" + this.getDate().get(GregorianCalendar.HOUR_OF_DAY) +
-                ", Minute=" + this.getDate().get(GregorianCalendar.MINUTE) +
-                ", Second=" + this.getDate().get(GregorianCalendar.SECOND) + "]";
-    }
+
+	public String toString()
+	{
+		return "CheckIn [DAY=" + this.getDate().get(GregorianCalendar.DAY_OF_MONTH) + ", Month="
+				+ (this.getDate().get(GregorianCalendar.MONTH) + 1) + ", Year="
+				+ this.getDate().get(GregorianCalendar.YEAR) + ", Hour="
+				+ this.getDate().get(GregorianCalendar.HOUR_OF_DAY) + ", Minute="
+				+ this.getDate().get(GregorianCalendar.MINUTE) + ", Second="
+				+ this.getDate().get(GregorianCalendar.SECOND) + "]";
+	}
 
 	@Override
-	public int compareTo(CheckIn o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(CheckIn o)
+	{
+		return o.getDate().compareTo(this.date);
+
 	}
-	
+
 }
--- app/src/main/java/nl/scalda/pasimo/service/StudentService.java
+++ app/src/main/java/nl/scalda/pasimo/service/StudentService.java
@@ -2,35 +2,39 @@ package nl.scalda.pasimo.service;
 
 import java.util.ArrayList;
 
-import nl.scalda.pasimo.controller.employeemanagement.StudentController;
 import nl.scalda.pasimo.model.employeemanagement.Student;
 import nl.scalda.pasimo.model.presenceregistration.CheckIn;
 
-public class StudentService {
+public class StudentService
+{
 	private static StudentService instance = null;
-	
-	
-	public void checkInData(Student z){
-		
-		ArrayList<CheckIn>tempCheckIns = new ArrayList<>();
+
+	public void checkInData(Student z)
+	{
+
+		ArrayList<CheckIn> tempCheckIns = new ArrayList<>();
 		CheckIn c = new CheckIn(0, 211543, 2017, 05, 13, 8, 30, 43);
 		CheckIn d = new CheckIn(0, 211543, 2017, 05, 16, 9, 05, 11);
 		tempCheckIns.add(c);
 		tempCheckIns.add(d);
-		
-			z.getStudentCheckIns().addAll(tempCheckIns);
-			System.out.println(z.studentCheckIns);
-			System.out.println(tempCheckIns);
+
+		z.getStudentCheckIns().addAll(tempCheckIns);
+		System.out.println(z.studentCheckIns);
+		System.out.println(tempCheckIns);
 	}
-	public static StudentService getInstance() {
-		if(instance == null) {
-	         instance = new StudentService();
-	      }
-	      return instance;
-   }
-	
-	public static void setInstance(StudentService instance) {
+
+	public static StudentService getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new StudentService();
+		}
+		return instance;
+	}
+
+	public static void setInstance(StudentService instance)
+	{
 		StudentService.instance = instance;
 	}
-	
+
 }
