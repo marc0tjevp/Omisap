734948bbbcd016cab629b188cfd747f0a06ead4d
LAPTOP-13ECKK85\Shaniva Werkman <SWerkman@student.scalda.nl>
2017-06-06 14:07:52 +0200

    started redoing datalayer, f'ed up the last one by working with a wrong student class

--- app/src/main/java/nl/scalda/pasimo/datalayer/interfaces/IDAOStudent.java
+++ app/src/main/java/nl/scalda/pasimo/datalayer/interfaces/IDAOStudent.java
@@ -1,11 +1,31 @@
 package nl.scalda.pasimo.datalayer.interfaces;
 
-import java.util.Set;
+import java.util.TreeSet;
 
+import nl.scalda.pasimo.model.employeemanagement.CoachGroup;
+import nl.scalda.pasimo.model.employeemanagement.EducationTeam;
 import nl.scalda.pasimo.model.employeemanagement.Student;
 
+import nl.scalda.pasimo.model.employeemanagement.LessonGroup;
+
 public interface IDAOStudent {
-	public void create(Student s);
 	
-	public Set<Student> readAll();
+	   public void create(Student student);
+
+	    public void update(Student s);
+
+	    public void delete(Student s);
+
+	    public TreeSet<Student> readAll();
+
+	    public Student readByOVNumber(int StudentOV);
+
+	    public TreeSet<Student> readAllForLessonGroup(LessonGroup l);
+	    
+	    public TreeSet<Student> readAllForCoachGroup(CoachGroup c);
+		
+		public EducationTeam getCurrentCoachGroupOfStudent(Student student);
+		
+		public EducationTeam getCurrentLessonGroupOfStudent(Student student);
+
 }
